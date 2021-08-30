1e5ef9ac7334048c2e47e4c8460de2227878e81d
Bia863086 <beatrizcampos2101@gmail.com>
2021-08-29 22:01:05 -0300

    Alterei a linha de comando padrão e coloquei uma apresentação

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace olaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá usuário, eu me chamo Adryely!");
+        }
+    }
+}
